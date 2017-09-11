8b2f9d0f6ed11b8ac232ca986fc0c435116efcee
Robert Laws <roblaws@me.com>
2017-09-11 16:25:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,10 @@
 require 'pry'
-require_relative './concerns/memorable.rb'
 
 class Song
-  extend Memorable
-
   attr_accessor :name
   attr_reader :artist
+
+  extend Memorable
 
   @@songs = []
 
